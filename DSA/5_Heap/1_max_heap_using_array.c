537db79735b56f3be5629170a4d2c43586b7caf8
phenomenalprince15 <84849566+phenomenalprince15@users.noreply.github.com>
2021-09-29 12:51:42 +0530

    Add files via upload

---
+++ DSA/5_Heap/1_max_heap_using_array.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+
+int size = 0;
+
+void swap(int *a, int *b)
+{
+    int temp = *b;
+    *b =  *a;
+    *a = temp;
+}
+
+void heapify(int array[], int size, int i)
+{
+    if (size == 1){
+        printf("Single element in array.\n");
+    }else{
+        int largest = i;
+        int l = 2*i+1;
+        int r = 2*i+2;
+        if(l<size && array[l]>array[largest])
+        {
+            largest = l;
+            //printf("largest is set to l.\n");
+        }
+        if(r<size && array[r]>array[largest])
+        {
+            largest = r;
+            //printf("largest is set to r.\n");
+        }
+        if(largest != i){
+            swap(&array[i],&array[largest]);
+            heapify(array,size,largest);
+        }
+    }
+}
+
+void insert(int array[], int new_num)
+{
+    if(size == 0){
+        array[0] = new_num;
+        size++;
+    }else{
+        array[size] = new_num;
+        size++;
+        for(int i=size/2-1; i>=0;i--)
+            heapify(array,size,i);
+    }
+}
+
+void delete(int array[], int num)
+{
+    int i=0;
+    for (i=0;i<size;i++)
+    {
+        if(num == array[i])
+            break;
+    }
+    swap(&array[i],&array[size-1]);
+    size--;
+    for (int i=size/2-1;i>=0;i--)
+        heapify(array,size,i);
+}
+
+void print(int array[], int size)
+{
+    for(int i=0;i<size;i++)
+        printf("%d ",array[i]);
+    printf("\n");
+}
+
+int main()
+{
+    int array[10];
+    insert(array,10);
+    print(array, size);
+    insert(array,20);
+    print(array, size);
+    insert(array,30);
+    print(array, size);
+    insert(array,40);
+    print(array, size);
+    insert(array,50);
+
+    printf("Max-Heap array :: ");
+    print(array, size);
+}
